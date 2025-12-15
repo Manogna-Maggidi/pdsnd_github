7ae0aaac66bc4cc3aaa9d2ce3bdfe3a20d4915b2
Manogna-Maggidi <manognamaggidi@gmail.com>
2025-12-15 16:50:10 +0530

    Refactor: improve startup message clarity

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 """Bikeshare analysis starter file."""
 
 def main():
-    print("Bikeshare project started")
+    print("Bikeshare project execution started")
 
 if __name__ == "__main__":
     main()
